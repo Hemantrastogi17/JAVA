c36ebf6012a22a130c8b659b1e4faa7277dd8cd4
Hemant <hemantrastogi17@gmail.com>
2021-03-23 23:16:31 +0530

    Java OOPS

---
+++ Java OOPS/Interfaces.java
@@ -0,0 +1,35 @@
+package com.company;
+interface biCycle{
+    int a = 45; //We can create properties in interfaces but these properties are final and can't be modified
+    void applyBreak(int decrement);
+    void speedUp(int increment);
+}
+interface blowHorn{
+    void blowHorn_mein_hu_na();
+    void bloHorn_dhoom();
+}
+class avonBicycle implements biCycle,blowHorn{
+    void blowHorn(){
+        System.out.println("PEEp PEEp");
+    }
+    public void applyBreak(int dercrement){
+        System.out.println("Decreasing speed by");
+    }
+    public void speedUp(int increment){
+        System.out.println("Increasing speed");
+    }
+    public void blowHorn_mein_hu_na(){
+        System.out.println("Mein hu na");
+    }
+    public void bloHorn_dhoom(){
+        System.out.println("Dhoom DHoom");
+    }
+}
+
+public class Interfaces {
+    public static void main(String[] args) {
+    avonBicycle a = new avonBicycle();
+    a.applyBreak(65);
+        System.out.println(a.a);
+    }
+}

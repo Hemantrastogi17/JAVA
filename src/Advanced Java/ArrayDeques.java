812eee4f3dfafe07f0a058a6d0a39f8b3200a81a
Firefist <firefist@Firefists-MacBook-Pro.local>
2022-09-07 11:23:06 +0530

    Collections Framwork

--- src/Advanced Java/ArrayDeques.java
+++ src/Advanced Java/ArrayDeques.java
@@ -1,5 +1,11 @@
-public class ArrayDeque {
+import java.util.*;
+public class ArrayDeques {
     public static void main(String[] args) {
-        ArrayDeque<Integer> ad = new ArrayDeque();
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(1);
+        ad.add(2);
+        ad.addFirst(0);
+        ad.getLast();
+
     }
 }
--- src/Advanced Java/LinkedLists.java
+++ src/Advanced Java/LinkedLists.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class LinkedLists {
+import java.util.*;
+public class LinkedLists {
+    public static void main(String[] args) {
+//        https://www.baeldung.com/java-array-deque
+       LinkedList<Integer> l1 = new LinkedList<>();
+       LinkedList<Integer> l2 = new LinkedList<>();
+       l1.add(1);
+       l1.add(2);
+       l2.add(3);
+       l2.add(4);
+       l1.addAll(l2);
+       l1.addLast(5);
+       l1.addFirst(0 );
+       for(int ele: l1)
+           System.out.println(ele);
+    }
 }
